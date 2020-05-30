d266439aab16dec825287bdc6a481df2ccf95c5a
Eldhose Kuriakose <56919546+EldhoseKuriakose@users.noreply.github.com>
2020-05-31 03:29:58 +0530

    commit

---
+++ Assets/Javascript/resumescript.js
@@ -0,0 +1,11 @@
+window.onscroll = function() {myFunction()};
+var header = document.getElementById("nav");
+var sticky = header.offsetTop;
+
+    function myFunction() {
+        if (window.pageYOffset > sticky) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    }
